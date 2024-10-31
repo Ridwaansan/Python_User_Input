4d4ef29c0251bcd28ce748e4396fbcbcaef4494c
Ridwaansan <sanusiridwaansanusi.gmail.com>
2024-10-31 11:18:07 +0100

    user details message

---
+++ user_input.py
@@ -0,0 +1,13 @@
+#ask the user for their name
+name = input("What is your name: ")
+
+#ask the user for their age 
+age = input("What is your age: ")
+
+#ask the user for their location
+location = input("Please enter your location: ")
+
+
+#print a personalized message with the user input
+print(f"Hello, {name}, you are {age} years old and live in {location}")
+
